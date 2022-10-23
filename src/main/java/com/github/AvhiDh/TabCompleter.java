d2c406e93d8661b3c4b9f70606f33bbdec0cd294
Abhinavd <abhinavd@pbssystems.com>
2022-10-23 13:58:51 -0600

    Update MTHTools version

--- src/main/java/com/github/AvhiDh/TabCompleter.java
+++ src/main/java/com/github/AvhiDh/TabCompleter.java
@@ -20,21 +20,25 @@ public class TabCompleter implements org.bukkit.command.TabCompleter {
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         List<String> li = new ArrayList<String>();
 
+        String arg;
+        if (args.length > 0) { arg = args[0].toLowerCase(); } else {arg = "";}
+
         if (cmd == Helpers.AvailableCmds.FREEZE) {
-            playerListCompleter(li);
+            playerListCompleter(li, arg);
         } else if (cmd == Helpers.AvailableCmds.DEATHSPOT) {
             if (sender.hasPermission("deathspot.admin")) {
-                li.add("reload");
+                if (arg.length() == 0 || arg.contains("reload")) { li.add("reload"); }
             }
-            playerListCompleter(li);
+            playerListCompleter(li, arg);
         }
 
         return li;
     }
 
-    private void playerListCompleter(List<String> li) {
+    private void playerListCompleter(List<String> li, String arg) {
         for (Player p : Bukkit.getOnlinePlayers()) {
-            li.add(p.getName());
+            String name = p.getName().toLowerCase();
+            if (arg.length() == 0 || name.contains(arg)) { li.add(p.getName()); }
         }
     }
 }
